808402e0a0590ed075393d53f898bdb0fb1e1342
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2023-09-09 00:40:53 +0300

    Improved path handling

--- scriptStarter.sh
+++ scriptStarter.sh
@@ -8,8 +8,16 @@ then
     return 1
 fi
 
+getAbsDirPath () {
+  echo "$(realpath $1 | xargs dirname)"
+}
+
+absScriptDirPath="$(getAbsDirPath $0)"
+absLauncherPath="$absScriptDirPath/sessionLauncher.go"
+absConfigPath="$(getAbsDirPath $configPath)/$(basename $configPath)"
+
 dir=$(grep -Po '"projectDir":.*?[^\\]",' $configPath | awk -F ':' '{print $2}' | cut -d "\"" -f 2)
-absolutePath=$(eval echo $dir)
+absoluteProjectPath=$(eval echo $dir)
 cd $absolutePath
 
-go run "$(dirname $configPath)/sessionLauncher.go" $configPath
+go run $absLauncherPath $absConfigPath
